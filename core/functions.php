74aeae9a213c7c6eba984fd3384d4a53f2f37d65
felgy <forginc@outlook.com>
2018-07-24 08:09:53 +0000

    Add comments to core functions

--- core/functions.php
+++ core/functions.php
@@ -1,17 +1,14 @@
 <?php
 
-// Autoload classes.
 spl_autoload_register(function ($class) {
     require_once ROOT . '/' . str_replace('\\', '/', $class) . '.php';
 });
 
-// Exception  handler.
 function exception_handler($e)
 {
     show_error($e->getMessage(), $e->getCode(), $e->getFile(), $e->getLine());
 }
 
-// Display errors.
 function show_error($message, $code, $file, $line)
 {
     http_response_code($code);
@@ -32,26 +29,28 @@ function show_error($message, $code, $file, $line)
     }
 }
 
-// Initiates the activity for generating the requested page.
+/**
+ * Initiates the activity for generating the requested page.
+ */
 function get_page()
 {
-    $route = router();
-    $controller = CONTROLLERS . '/' . $route['controller'] . '.con.php';
-    $action = $route['action'];
+    $controller = CONTROLLERS . '/' . controller() . '.con.php';
+    $action = action();
     if (is_file($controller)) {
         require_once $controller;
         if (function_exists($action)) {
             return $action();
         } else {
-            throw new Exception('Action not found!', 404);
+            throw new Exception('Action "' . $action . '" not found!', 404);
         }
     } else {
-        throw new Exception('Controller not found!', 404);
+        throw new Exception('Controller "' . controller() . '" not found!', 404);
     }
-
 }
 
-// Strike from request controller and model.
+/**
+ * Cut GET parameters from request and return controller and model.
+ */
 function router()
 {
     $route = [
@@ -69,55 +68,81 @@ function router()
     return $route;
 }
 
-// Fill in the template with the data and place the result in the variable.
+/**
+ * Fill in the template with the data and return the result in the string.
+ * @param $data 
+ * @param $controller
+ * @param $template
+ */
 function view($data, $controller, $template)
 {
     $template = TEMPLATES . '/' . $controller . '/' . $template . '.tpl.php';
+    $action = 'get_' . $template . '_data';
     if (is_file($template)) {
         extract($data);
         ob_start();
         require_once $template;
         return ob_get_clean();
     } else {
-        throw new Exception('Template for this action not found!', 404);
+        throw new Exception('Template for action "' . action() . '" not found!', 404);
     }
 }
 
-// Put together templates and return the finished page.
+/**
+ * Put together prepared templates and return the finished page.
+ * Array $data is data for the base template and part of the content.
+ * @param $template | template for current view content part.
+ */
 function render($template)
 {
     $data = get_data($template);
-    $data['content'] = view($data, controller_name(), $template);
+    $data['content'] = view($data, controller(), $template);
     return view($data, 'main', BASE_LAYOUT);
 }
 
-// Returns controller name.
-function controller_name()
+/**
+ * Return current controller name.
+ */
+function controller()
 {
     $route = router();
     return $route['controller'];
 }
 
-// Returns model file path.
-function model_file()
+/**
+ * Return current action name.
+ */
+function action()
 {
-    $model = MODELS . '/' . controller_name() . '.mod.php';
+    $route = router();
+    return $route['action'];
+}
+
+/**
+ *  Returns model file path.
+ */
+function model()
+{
+    $model = MODELS . '/' . controller() . '.mod.php';
     if (is_file($model)) {
         return $model;
     } else {
-        throw new Exception('Model for controller "' . controller_name() . '" not found!');
+        throw new Exception('Model for controller "' . controller() . '" not found!');
     }
 }
 
-// Prepare and return the relevant data array.
+/**
+ * Prepare the function name and call to get template data from the model.
+ * @param $template
+ */
 function get_data($template)
 {
-    require_once model_file();
-    $function = 'get_' . $template . '_data';
-    if (function_exists($function)) {
-        return $function();
+    require_once model();
+    $action = 'get_' . $template . '_data';
+    if (function_exists($action)) {
+        return $action();
     } else {
-        throw new Exception('Data for this controller not found!', 404);
+        throw new Exception('Data for action "' . $action . '" not found!', 404);
     }
 }
 
